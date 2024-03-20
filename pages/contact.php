c8276be085a40e61406b2be93ddf7083245326cb
Wojanix <wojtek.burek@gmail.com>
2024-03-20 21:55:24 +0100

    Contact form ctrl c v

--- pages/contact.php
+++ pages/contact.php
@@ -6,6 +6,7 @@
     <title>Kontakt</title>
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/nav.css">
+    <link rel="stylesheet" href="../css/contact.css">
 </head>
 <body>
 <div class="navbar">
@@ -14,6 +15,49 @@
   <a href="map.php"><i class="fa fa-fw fa-envelope"></i> Mapa</a>
   <a href="add.php"><i class="fa fa-fw fa-user"></i> Dodaj</a>
   <a class="active" href="contact.php"><i class="fa fa-fw fa-user"></i> Kontakt</a>
+</div>
+<div class="card">
+  <h2>Contact Us</h2>
+  <div class="row">
+    <div class="col">
+      <div class="form-group">
+        <label>First Name</label>
+        <input type="text">
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="form-group">
+        <label>Surname</label>
+        <input type="text">
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="form-group">
+        <label>Email</label>
+        <input type="text">
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="form-group">
+        <label>Phone</label>
+        <input type="text">
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="form-group">
+        <label>Message</label>
+        <textarea></textarea>
+      </div>
+    </div>
+
+    <div class="col">
+      <input type="submit" value="Submit">
+    </div>
+  </div>
 </div>
 </body>
 </html>
