3829e96932dcde88249136341b53588755e0ee7e
Wojanix <wojtek.burek@gmail.com>
2024-09-09 12:21:32 +0200

    Reels
    maybe someday

--- components/header.php
+++ components/header.php
@@ -10,6 +10,7 @@
   <a class="active" href="./index.php"><i class="fa fa-fw fa-home"></i> Oblicz</a>
   <a href="./ranking.php"><i class="fa fa-fw fa-envelope"></i> Ranking</a>
   <a href="./map.php"><i class="fa fa-fw fa-envelope"></i> Mapa</a>
+  <a href="./reels.php"><i class="fa fa-fw fa-envelope"></i> Reels</a>
   <a href="./add.php"><i class="fa fa-fw fa-user"></i> Dodaj</a>
   <a href="./contact.php"><i class="fa fa-fw fa-user"></i> Kontakt</a>
 </div>
