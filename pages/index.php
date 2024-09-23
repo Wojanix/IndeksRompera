6ba27b6809dff6a85dbe91f020dbe0eee5823d3a
Wojanix <wojtek.burek@gmail.com>
2024-09-23 11:13:51 +0200

    some shit

--- pages/index.php
+++ pages/index.php
@@ -37,7 +37,11 @@ echo '<script type="text/javascript">',
 'connected();',
 '</script>';
 $ir = 0;
-if(floatval($_GET["price"])>0 &&floatval($_GET["percent"])>0 &&floatval($_GET["quantity"])>0 ){$ir= bcdiv(floatval($_GET["quantity"])*floatval($_GET["percent"])*0.01/floatval($_GET["price"]),1,2);} 
+if(isset($_GET["price"])){
+  if(floatval($_GET["price"])>0 &&floatval($_GET["percent"])>0 &&floatval($_GET["quantity"])>0 ){
+    $ir= bcdiv(floatval($_GET["quantity"])*floatval($_GET["percent"])*0.01/floatval($_GET["price"]),1,2);
+  } 
+}
 $beforeRecord = mysqli_query($conn, "SELECT * from drink where ir>=".$ir." order by ir asc limit 1");
 $aferRecord = mysqli_query($conn, "SELECT * from drink where ir<".$ir." order by ir desc limit 1");
 ?>
